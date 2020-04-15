0d6adff7274dc5b05249f7eb10fa5529c75b374d
Chen Gazit <gazit.chen@gmail.com>
2020-04-15 17:43:02 +0300

    welcome message

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import { user as userSelector } from '../../state/auth/selectors';
 
 const Home = () => {
-    return <Page>This is the homepage</Page>;
+    const user = useSelector(userSelector);
+    return (
+        <Page>
+            <WelcomeMessage>Welcome back, {user.firstname}!</WelcomeMessage>
+        </Page>
+    );
 };
 
 const Page = styled.div`
+    padding: 30px;
+    margin-left: 220px;
     background-color: ${(props) => props.theme.colors.appBackground};
     height: 100%;
-    width: 100%;
+`;
+
+const WelcomeMessage = styled.div`
+    font-size: 40px;
+    color: ${(props) => props.theme.colors.white};
 `;
 
 export default Home;
